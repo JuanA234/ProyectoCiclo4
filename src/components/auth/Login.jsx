2ae80b2b2dc6794070df10de3eee3c2a60db2fcc
JuanA <juanandreslujan30@gmail.com>
2021-12-18 16:56:57 -0500

    agregar pagina de registro y login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Input from 'components/shared/Input'
+import ButtonLoading from 'components/shared/ButtonLoading'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <div className='flex flex-col items-center justify-center w-full h-full p-10'>
+        <h1 className='text-xl font-bold text-gray-900'>Iniciar sesión</h1>
+        <form
+          className='flex flex-col'
+        //   onSubmit={submitForm}
+        //   onChange={updateFormData}
+        //   ref={form}
+        >
+          <Input name='correo' type='email' label='Correo' required />
+          <Input name='password' type='password' label='Contraseña' required />
+          <ButtonLoading
+            // disabled={Object.keys(formData).length === 0}
+            // loading={mutationLoading}
+            text='Iniciar Sesión'
+          />
+        </form>
+        <span>¿No tienes una cuenta?</span>
+        <Link to='/auth/register'>
+          <span className='text-blue-700'>Regístrate</span>
+        </Link>
+      </div>
     )
 }
 
